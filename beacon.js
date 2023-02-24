eb101819212152ce714a9e2b5befe8abac3fac3e
ziadNabill <126259450+ziadNabill@users.noreply.github.com>
2023-02-24 14:54:40 +0000

    Add files via upload

---
+++ beacon.js
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <title>Just a moment...</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta name="robots" content="noindex,nofollow">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link href="/cdn-cgi/styles/challenges.css" rel="stylesheet">
+    <meta http-equiv="refresh" content="35">
+
+</head>
+<body class="no-js">
+    <div class="main-wrapper" role="main">
+    <div class="main-content">
+        <h1 class="zone-name-title h1">
+            <img class="heading-favicon" src="/favicon.ico" alt="Icon for performance.radar.cloudflare.com"
+                 onerror="this.onerror=null;this.parentNode.removeChild(this)">
+            performance.radar.cloudflare.com
+        </h1>
+        <h2 class="h2" id="challenge-running">
+            Checking if the site connection is secure
+        </h2>
+        <noscript>
+            <div id="challenge-error-title">
+                <div class="h2">
+                    <span class="icon-wrapper">
+                        <div class="heading-icon warning-icon"></div>
+                    </span>
+                    <span id="challenge-error-text">
+                        Enable JavaScript and cookies to continue
+                    </span>
+                </div>
+            </div>
+        </noscript>
+        <div id="trk_jschal_js" style="display:none;background-image:url('/cdn-cgi/images/trace/jsch/nojs/transparent.gif?ray=79e9073dec740c44')"></div>
+        <div id="challenge-body-text" class="core-msg spacer">
+            performance.radar.cloudflare.com needs to review the security of your connection before proceeding.
+        </div>
+        <form id="challenge-form" action="/beacon.js?__cf_chl_f_tk=NkujrG5TBcyaMGCE8btUinVDxGAUMZP0lFPstYQ6iXU-1677250380-0-gaNycGzNCOU" method="POST" enctype="application/x-www-form-urlencoded">
+            <input type="hidden" name="md" value="3e6T_0uPircGeTJESdDMqgU7ggtvWf2YiJS86OE.1WE-1677250380-0-ASUH9haCcVBkiE70jZAI9smQi71qTOvvnKzoeYldCSGlvk5ZK8ziyQ68XKhZ6E7xZjFgk1C5i4z9KoA17bVdftEbDl8QDOVuLJCue-aJ6G-1n6nhOOto-hWdgFzoY1LKF5nnBwDukXbY8AwuayvvE9-S5s-l5Ls4YeRjvyNeeDrl2LyFLdfF9rbzigjHHpHFhQnmP54zLGexmpMPSkSSCQXGbgW40u54067rxx1tNj4_O1oxLiyr3Ij3DkEHR0fO9jM23d_T6VYp4n1XJjJbSS6okswBPdIkh7T_zdHNskNVyCYgvL1Js2qK0KWvaOp2QXVss1K6oSrTQ71-qtUEVTvE8fa7phUlwr-8nqM2ChMY8hEqq9Czr8tI-8TwDlqvJSxYqJnKpKO438G6XnfrlfR7rmS9pDOTkOkgEvWT4S1BPpUbFFa_AELQpW4qDJs0HXFxGrTu3_VRzvRDp_BmbJUJdJXPYgMK8f03PRrsuPUAlhBGayTEOSVA2VyK3WGpi6LMYdzs1D8CBt2otO90qiWIdK5x6azOLsCTJ-5NYdKp4pAVZ9vuyAOU2G4ysaKVo0ry2TQzCVOnmhR-uJRDClxWt4kF26iCd1dV-QUBdeFzvldkotJGk30Cx2Et_gAA6lY_Na-TBA9Rkxfp2itOE07sB4LJfAAxNAeQNYcGFIKysxlevdUFunLOIQjraKyIc3Z9RiQhtqVjQ5r-0oGq4TUknqKVNiGMO7WkagIi1e_8gP6B-piBzc6nLAA_N1VdS3_uSQjTlOjvJWpSNoA7JqYpoNFgMDVz6Pm_55MJYjZqyAHoBG_IFfB2-7SXkWD1LJKGUe530w-qQcV23257JsmN--07yDwSvkmKG7poGymX8UY0Mj8qQmdR2s3DTzta-kFdafSO32C931i_oK2eDedeKRB-S7quwrbRncNv69shOw-94HbpXeploUBnxGgSn6HL7Jt0GhzY0eqdl56NuLRjeSdMtpMh9HCjTekUu64tC1fGBLVx2zk8uMbzwYJ0FmT7fuoLkuh9use3jwclCrs2_YWIEyeHeTo-FalQZu2iX6wrb_0ZjQNjNwYoIEyV-r3YNw1JmDlQS6odsXwGkLEYhqmtjutYI4GIiMHpXWZZDpC9bK3rJK5v8IAvDSBCmRjpOY5P5Jm9lvx6ys8nrtIDbjf4yuXJfPNvrxswKtBYDQXQW7Sur-nWhwFtDykur9fQC8DfkxMj5uk1ezi82vUGfdJkwPLk0f_AMdS10AmjGDI4rttzj5yNxXvHoOqSWO6czVvjHHhIYJrS8l5CTn4_yPZTMVv7ntkt2XCxZhyNdHnLQPked1VBNVEVstMh2mlN5kO8zGN_gkOVNm_xPGaY5alayu4UIs5484UzSEKGK0v6bkoS4nLEJ2kveX1q2iNFcbyhJlD64p3IU_FgGkLfsHKnjShk1kbgZlCdOaWOjY5WOuP5K0luGRC-k_jYlFuvEac0mlPW3UkST6kA4JyKPhEeYGRbLCr49MpyoDtYJyT-caSzhEn-JXOP-PWvNt0XURk6n4fX_QVaS7HJoj2NpisGseDIbPWghu4hYDyf-Li33hGrDi2ayYxNBMP27I_tGO3nnEYb-MWRyXibbsb686a9jQ8hPqz4rcPR68L3xH6yH8yWnFIJSIBAYOastrr8mijaKnutQeMXirzgW4RL1583DPW_PpIH2_eyu4n_J4IG4qG0EIgqyGlWUg8ji7haaFmstTVZXk2TnHfTapZd8EHOlYDwAaZOVMZ1bvJtdqK4KzoWRnM_xIk7tESiKOcwnPIh3lmxewQH6k4juhG7StStHk8cv-NiKk0R22lOt4BAtg-M93xVB4RdQbqJi_HgJSSv1CfUVb3MD8aNOLzYesW30qIy_wl2yyzHrsQygtOrsttQPUDDb_zJ31I7EqI9dzHHoYHOtYl6FKf6Tp-x6HS_JC8ux80EUTuHLaNdtQV_3eGhzYLA22Mv1ZDFy8bTDmUJiCwP234k4CGCEIwI9LClYC3QUW-faKpH3bDOXc13ypHdAfcFcOcf5_8pGWHvha8TYJ0bhZcEg-xCHlLQ9o1gGkAYFYjdmTi0D_6OxUupXbZDLIm9UgEES3Ea284RdZGEooYumW7smDVaFgP-K6MMPcoip34_FtvNObhLK4kPNhwH2Esmh7V_TMBUuLfhmV5KlO0ZXvPh9hXynlCYxzte_g67FHwjhks8aMyH">
+        </form>
+    </div>
+</div>
+<script>
+    (function(){
+        window._cf_chl_opt={
+            cvId: '2',
+            cZone: 'performance.radar.cloudflare.com',
+            cType: 'non-interactive',
+            cNounce: '40542',
+            cRay: '79e9073dec740c44',
+            cHash: 'd9644720ee33689',
+            cUPMDTk: "\/beacon.js?__cf_chl_tk=NkujrG5TBcyaMGCE8btUinVDxGAUMZP0lFPstYQ6iXU-1677250380-0-gaNycGzNCOU",
+            cFPWv: 'g',
+            cTTimeMs: '1000',
+            cMTimeMs: '60000',
+            cTplV: 4,
+            cTplB: 'cf',
+            cRq: {
+                ru: 'aHR0cHM6Ly9wZXJmb3JtYW5jZS5yYWRhci5jbG91ZGZsYXJlLmNvbS9iZWFjb24uanM=',
+                ra: 'cHl0aG9uLXJlcXVlc3RzLzIuMjguMg==',
+                rm: 'R0VU',
+                d: 'LCoxheWqKg5og+03pdnPdetsR8G+65xsq3+aJsKFWJMoCqyqjGZ7DnEP3CQuo0KXr/dP3A933xbV2g2JYQdWp8CSH4yFToguZ9kl07Iu62B6eoPVRDfJ3X2ydJRDFinQBVffXk0DgU8Q6dDuSwV2k3wW/fWkrMocuxZIhxVR9rZKCxdhR2cyGCGsDkzZsglaw6r1MaUOAmyHhTeKK3Oll7x+4w9V5eduXQiYsr2EeHQmWimJ8v6pCRyhJiv79qifVXvrK2ZwNp+qew6VIoY3IQkCGBjTqcPgXKD0VtrYXHM67BnJrgSWOqnnSjTUaLoSUJVgjXjktk1ECgNbDQHT2yaH8ZY3nVbGkW0qX9uhiniR3zhzPYQ3tY+gqTplqV4NAULLNeEcdppyQycbIncNDBEQrXFp4l8YP0rzYZGhNXffDmK5Jb4xWVAcQ8IZcl8zTZggJ75NPsOLMqO+LhZiVCKsx6eZb6Yc9iuJ8uSaCfoTmyTm+FrXbMH51GlXXwjpuO4lhPZ8aPeQmbVUKd8q+qZLDqCRGuwisj6O0ZXKbQAJbKvg8OY6RvgIR8JoJnG7Ro3WAfX/1WwP1191Z/GfdzFZLfkUNisRfcQCroOubM011oepnqs4rk91uTENx2WLRqzdTau4N1KH+1Bws39FJy6/mHP3e8Y6tGqYTh3TTy8I2YFJ2JMWOsuIlNbDb7vC0jmVhrfI+TYBrQx2llH3xg==',
+                t: 'MTY3NzI1MDM4MC40NjUwMDA=',
+                m: 'xTuMIkwF/Xui4U4asnZjn0pO9CrBc+l1qq2czxN8tRE=',
+                i1: 'lXeHPiMysxEp9hcdt0nFkw==',
+                i2: 'I7LGiBM0ok5aCpwYQ6oYDg==',
+                zh: 'aUaxZ8qdv0fXBZ54M8uAxySUqysBfFxLCu1NuWLxnHM=',
+                uh: 'neMFmDCz0LRc0/Wea+5x0IhJieHbzcQVSkMgFkgnrjc=',
+                hh: 'ksLUahdQFjidR8j9ZICxx0YyJHg0Yj3VmSCq5TiMq8A=',
+            }
+        };
+        var trkjs = document.createElement('img');
+        trkjs.setAttribute('src', '/cdn-cgi/images/trace/jsch/js/transparent.gif?ray=79e9073dec740c44');
+        trkjs.setAttribute('alt', '');
+        trkjs.setAttribute('style', 'display: none');
+        document.body.appendChild(trkjs);
+        var cpo = document.createElement('script');
+        cpo.src = '/cdn-cgi/challenge-platform/h/g/orchestrate/jsch/v1?ray=79e9073dec740c44';
+        window._cf_chl_opt.cOgUHash = location.hash === '' && location.href.indexOf('#') !== -1 ? '#' : location.hash;
+        window._cf_chl_opt.cOgUQuery = location.search === '' && location.href.slice(0, location.href.length - window._cf_chl_opt.cOgUHash.length).indexOf('?') !== -1 ? '?' : location.search;
+        if (window.history && window.history.replaceState) {
+            var ogU = location.pathname + window._cf_chl_opt.cOgUQuery + window._cf_chl_opt.cOgUHash;
+            history.replaceState(null, null, "\/beacon.js?__cf_chl_rt_tk=NkujrG5TBcyaMGCE8btUinVDxGAUMZP0lFPstYQ6iXU-1677250380-0-gaNycGzNCOU" + window._cf_chl_opt.cOgUHash);
+            cpo.onload = function() {
+                history.replaceState(null, null, ogU);
+            };
+        }
+        document.getElementsByTagName('head')[0].appendChild(cpo);
+    }());
+</script>
+
+
+    <div class="footer" role="contentinfo">
+        <div class="footer-inner">
+            <div class="clearfix diagnostic-wrapper">
+                <div class="ray-id">Ray ID: <code>79e9073dec740c44</code></div>
+            </div>
+            <div class="text-center" id="footer-text">Performance &amp; security by <a rel="noopener noreferrer" href="https://www.cloudflare.com?utm_source=challenge&utm_campaign=j" target="_blank">Cloudflare</a></div>
+        </div>
+    </div>
+</body>
+</html>
--- translate.py
+++ translate.py
@@ -1,35 +1,54 @@
+import os
 import requests
 from bs4 import BeautifulSoup
 from googletrans import Translator
 
-def translate_text(text, target_language):
-    translator = Translator()
-    translation = translator.translate(text, dest=target_language)
-    return translation.text
+def download_file(url):
+    response = requests.get(url)
+    filename = os.path.basename(url)
+    with open(filename, 'wb') as file:
+        file.write(response.content)
+    return filename
 
-
-# scraping the website using beaytifulSoup library
-url = 'https://www.classcentral.com/' # Replace with the URL of your live website
-target_language = 'hi' # replace 'hi' with your desired language code
+url = 'https://www.classcentral.com' # Replace with the URL of your live website
+target_language = 'hi' # Replace 'hi' with your desired language code
 
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
-# Find all HTML elements that contain text content
-elements = soup.find_all(['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'span', 'a', 'li', 'td'])
+# Find all CSS and JavaScript files and download them
+css_links = [link['href'] for link in soup.find_all('link', rel='stylesheet')]
+js_links = [script['src'] for script in soup.find_all('script', src=True)]
 
-# Loop through all elements and translate their content
-for element in elements:
-    # Check if the element contains any child elements
-    if element.find():
+for link in css_links + js_links:
+    if link.startswith('//'):
+        link = 'https:' + link
+    elif link.startswith('/'):
+        link = url + link
+    download_file(link)
+
+# Loop through all HTML elements and translate their text content
+for element in soup.find_all():
+    # Skip elements that don't have any text content or are script or style tags
+    if element.name in ['script', 'style'] or not element.string:
         continue
-    # Translate the text content
+    # Translate text content
     translator = Translator()
-    translation = translator.translate(element.text, dest=target_language)
-    translated_text = translation.text
-    # Replace the original text content with the translated text content
-    element.string.replace_with(translated_text)
+    translation = translator.translate(element.string, dest=target_language)
+    element.string = translation.text
 
-# Write the translated HTML content to a file
+# Replace links to CSS and JavaScript files with the actual file content
+for link in css_links + js_links:
+    filename = os.path.basename(link)
+    with open(filename, 'r', encoding='utf-8') as file:
+        file_content = file.read()
+    css_element = soup.find('link', href=link)
+    if css_element is not None:
+        css_element['href'] = filename
+    js_element = soup.find('script', src=link)
+    if js_element is not None:
+        js_element['src'] = filename
+
+# Write translated HTML to file
 with open('translated.html', 'w', encoding='utf-8') as file:
-    file.write(soup.prettify())
+    file.write(str(soup))
